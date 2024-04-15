d9d3f6b504cfb4ee1d86e8a335d5f117fc03e63b
keal khondekar <kewalkhondekar25@gmail.com>
2024-04-16 03:58:35 +0530

    feat: prisma singleton added

--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,30 +1,43 @@
-import { PrismaClient } from "@prisma/client"
 import { log } from "console"
 import { NextRequest, NextResponse } from "next/server"
-
-const client = new PrismaClient();
+import client from "@/db"
 
 export const GET = async () => {
-    const response = await client.user.findMany({})
-    return NextResponse.json(response)
+    try {
+        const response = await client.user.findMany({})
+        return NextResponse.json(response)
+    } catch (error) {
+        NextResponse.json({
+            message: "Error while fetching users"
+        },{
+            status: 411
+        })
+    }
 };
 
 export const POST = async (req: NextRequest) => {
-    //extract body.
-    const body = await req.json();
-    //headers
-    console.log(req.headers.get("authorization"));
-    //query params
-    console.log(req.nextUrl.searchParams.get("name"));
-    
-    // store in db
-    await client.user.create({
-        data: {
-            username: body.username,
-            password: body.password
-        }
-    });
-    return NextResponse.json({
-       message: "signin completed"
-    })
+    try {
+        //extract body
+        const body = await req.json();
+        //headers
+        console.log(req.headers.get("authorization"));
+        //query params
+        console.log(req.nextUrl.searchParams.get("name"));
+        // store in db
+        await client.user.create({
+            data: {
+                username: body.username,
+                password: body.password
+            }
+        });
+        return NextResponse.json({
+           message: "signin completed"
+        })
+    } catch (error) {
+        NextResponse.json({
+            message: "Error while signing up"
+        },{
+            status: 411
+        })
+    }
 };
