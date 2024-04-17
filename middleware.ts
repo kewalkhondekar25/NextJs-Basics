3d15f33d9b59024b696c2b34f4eb7ff6c53188a0
keal khondekar <kewalkhondekar25@gmail.com>
2024-04-18 01:38:37 +0530

    feat: custom middlewares added

--- middleware.ts
+++ middleware.ts
@@ -2,11 +2,14 @@ import { NextRequest, NextResponse } from "next/server";
 
 let count = 0;
 export const middleware = (req: NextRequest) => {
-    count++;
-    console.log(count);
-    return NextResponse.next();
+    // count++;
+    // console.log(count);
+    if(req.nextUrl.pathname.startsWith("/signin")){
+        return NextResponse.redirect(new URL("/signup", req.url))
+    }
+    // return NextResponse.next();
 }
 
-export const config = {
-    matcher: "/signup"
-}
+// export const config = {
+//     matcher: "/signup"
+// }
