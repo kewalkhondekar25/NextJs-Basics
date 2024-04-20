4227a1e19652dd1bdc032810b8edd752f8c5c4b5
keal khondekar <kewalkhondekar25@gmail.com>
2024-04-21 03:20:37 +0530

    feat: navbar added

---
+++ app/client/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      this is client
+    </div>
+  )
+}
+
+export default page
---
+++ components/Navbar.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link'
+import { it } from 'node:test'
+import React from 'react'
+
+let menu = [
+    {href: "/client", label: "client"},
+    {href: "/drinks", label: "drinks"},
+    {href: "/tasks", label: "tasks"},
+    {href: "/query", label: "query"}
+]
+
+const Navbar = () => {
+  return (
+    <div className='bg-slate-200 py-4'>
+      <div>
+        <ul className='flex mx-3 gap-3'>
+            {
+                menu.map(item => {
+                    return(
+                        <li>
+                            <Link href={item.href} key={item.label}
+                            className='capitalize'>{item.label}</Link>
+                        </li>
+                    )
+                })
+            }
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
