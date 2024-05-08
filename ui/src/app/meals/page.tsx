a64fc9ebee24049c04d8d609c043307201a5b4a4
keal khondekar <kewalkhondekar25@gmail.com>
2024-05-08 20:10:35 +0530

    feat: layout added

--- ui/src/app/meals/page.tsx
+++ ui/src/app/meals/page.tsx
@@ -1,4 +1,6 @@
 import { log } from 'console';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react'
 
 interface DataType {
@@ -13,20 +15,24 @@ interface DataType {
 
 const page = async () => {
   const response = await fetch("https://vegan-meals-api.vercel.app/api/v1/meals");
-  if(!response.ok){
+  if (!response.ok) {
     throw new Error("failed to fetch meals")
   }
   const data: DataType[] = await response.json();
   console.log(data);
-  
+
   return (
-    <div>
+    <div className='flex flex-row flex-wrap mt-5 justify-center place-items-center'>
       {
         data.map(item => {
-          return(
-            <ul>
-              <li>{item.title}</li>
-            </ul>
+          return (
+            <div className="" key={item._id}>
+              <Link href="/meals/meals-info">
+                <Image
+                  className='ml-5 mt-5 rounded-[5px]'
+                  src={item.image} alt="img" height={200} width={200} />
+              </Link>
+            </div>
           )
         })
       }
