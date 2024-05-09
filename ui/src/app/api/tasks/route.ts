7cc917f8449cbbf90ae204fdadec8a46873454b1
keal khondekar <kewalkhondekar25@gmail.com>
2024-05-10 00:57:11 +0530

    feat: POST route handler added

--- ui/src/app/api/tasks/route.ts
+++ ui/src/app/api/tasks/route.ts
@@ -1,6 +1,21 @@
+import { NextRequest } from "next/server";
 import prisma from "../../../../utils/db";
+
+interface TaskType {
+  content: string
+}
 
 export const GET = async () => {
   const tasks = await prisma.task.findMany();
   return Response.json({data: tasks})
+}
+
+export const POST = async (request: NextRequest) => {
+  const data: Promise<TaskType> = await request.json();
+  const response = await prisma.task.create({
+    data: {
+      content: (await data).content
+    }
+  });
+  return Response.json({data: response})
 }
